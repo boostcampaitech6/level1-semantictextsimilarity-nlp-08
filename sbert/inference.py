5ebda3564e0e5a6652a7975768959d753b83b29f
soonyear <avatarhwang@gmail.com>
2023-12-18 01:48:41 +0000

    inference model load from checkpoint

--- sbert/inference.py
+++ sbert/inference.py
@@ -7,13 +7,15 @@ import pandas as pd
 import torch
 import pytorch_lightning as pl
 
+import wandb
+
 if __name__ == '__main__':
     # 하이퍼 파라미터 등 각종 설정값을 입력받습니다
     # 터미널 실행 예시 : python3 run.py --batch_size=64 ...
     # 실행 시 '--batch_size=64' 같은 인자를 입력하지 않으면 default 값이 기본으로 실행됩니다
     parser = argparse.ArgumentParser()
     parser.add_argument('--model_name', default='jhgan/ko-sroberta-multitask', type=str)
-    parser.add_argument('--batch_size', default=8, type=int)
+    parser.add_argument('--batch_size', default=128, type=int)
     parser.add_argument('--max_epoch', default=1, type=int)
     parser.add_argument('--shuffle', default=True)
     parser.add_argument('--learning_rate', default=1e-5, type=float)
@@ -32,7 +34,11 @@ if __name__ == '__main__':
 
     # Inference part
     # 저장된 모델로 예측을 진행합니다.
-    model = torch.load('model.pt')
+    run = wandb.init()
+    artifact = run.use_artifact('catwave/lightning_logs/model-3eznbo63:v0', type='model')
+    artifact_dir = artifact.download()
+    model = Model.load_from_checkpoint(artifact_dir + '/model.ckpt')
+    #model = torch.load('model.pt')
     predictions = trainer.predict(model=model, datamodule=dataloader)
 
     # 예측된 결과를 형식에 맞게 반올림하여 준비합니다.
